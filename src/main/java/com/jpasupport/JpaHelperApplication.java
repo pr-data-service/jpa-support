e0dfa82425cc03803442064423a04d9afaa6e82d
Pradyut Sarkar <85090543+pr-data-service@users.noreply.github.com>
2023-02-10 21:49:50 +0530

    code changes

--- src/main/java/com/jpasupport/JpaHelperApplication.java
+++ src/main/java/com/jpasupport/JpaHelperApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@ComponentScan({"any package can mention here", "com.in28minutes.springboot.basics.springbootin10steps","com.in28minutes.springboot.somethingelse"})
 public class JpaHelperApplication {
 
 	public static void main(String[] args) {
